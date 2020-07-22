a680d86398cf2f80d25b824d81c9c9cb94bd22cb
dwragon <36308552+dwragon@users.noreply.github.com>
2020-07-22 17:44:40 +0300

    Did just a simple scan

--- get_app.py
+++ get_app.py
@@ -1,6 +1,15 @@
 import winapps
-
-
+# Сделать запись установленых приложений в файл
+# Передать установленые браузеры в возврате для обработки
+app_list=[]
 def get_apps():
+
     for app in winapps.list_installed():
-        print(app)
+        app_list.append(str(app)) #все приложения записуются в апп лист
+    #     print(app)
+
+#def scan_apps():
+
+
+
+
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import winapps
 import get_app
 
-print(get_app.get_apps())
+print(get_app.scan_apps())
