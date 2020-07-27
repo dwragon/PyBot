addd72cc2c870abd7e263f0d1ab66ad76f5f8ba2
dwragon <36308552+dwragon@users.noreply.github.com>
2020-07-27 12:41:57 +0300

    Вот теперь точно работает отправка файлов по почте

--- send_file.py
+++ send_file.py
@@ -1,73 +1,49 @@
-# -*- coding: utf-8 -*-
-import os
-import smtplib
-import sys
-from configparser import ConfigParser
+import email, smtplib, ssl
+
 from email import encoders
-from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
-from email.utils import formatdate
+from email.mime.text import MIMEText
 
-# smtpObj = smtplib.SMTP('smtp.gmail.com',587)
-# smtpObj.starttls()
-# smtpObj.login('kurwapawa@gmail.com','bereg_srednii')
-# smtpObj.sendmail("kurwapawa@gmail.com","kurwapawa@gmail.com","theme","fileB.txt"
+subject = "An email with attachment from Python"
+body = "This is an email with attachment sent from Python"
+sender_email = "kurwapawa@gmail.com"
+receiver_email = "kurwapawa@gmail.com"
+password = ("bereg_srednii")
 
-def send_files(subject,body_text,to_emails,cc_emails,bbc_ebails,file_to_attach):
-    #Отправка информации с помощью почты
-    base_path = os.path.dirname(os.path.abspath(__file__))
-    config_path = os.path.join(base_path,"email.ini")
-    header = 'Content-Disposion','attchment; filename="%s"'%file_to_attach
+# Это для составного сообщения
+message = MIMEMultipart()
+message["From"] = sender_email
+message["To"] = receiver_email
+message["Subject"] = subject
+message["Bcc"] = receiver_email  # без этого не работает
 
-    #get the config
-    if os.path.exists(config_path):
-        cfg = ConfigParser()
-        cfg.read(config_path)
-    else:
-        print("Config not found Exiting!")
-        sys.exit(1)
-    #extract server and from_addr from config
-    host = cfg.get("smtp","server")
-    from_addr = cfg.get("smtp","from_addr")
+# Тело мыла
+message.attach(MIMEText(body, "plain"))
 
-    #create the message
-    msg = MIMEMultipart()
-    msg["from"] = from_addr
-    msg["Subject"] = subject
-    msg["Date"] = formatdate(localtime=True)
+filename = "fileb.txt"  # Указание названия файла и дериктории
 
-    if body_text:
-        msg.attach(MIMEText(body_text))
-    msg["To"] = ','.join(to_emails)
-    msg["cc"] = ','.join(cc_emails)
+# Открытие файла
+with open(filename, "rb") as attachment:
 
-    attachment = MIMEBase('application',"octet-stream")
+    part = MIMEBase("application", "octet-stream")
+    part.set_payload(attachment.read())
 
-    try:
-        with open(file_to_attach,"rb") as fh:
-            data = fh.read()
 
-        attachment.set_payload(data)
-        encoders.encode_base64(attachment)
-        attachment.add_header(*header)
-        msg.attach(attachment)
-    except IOError:
-        msg= "error opening attachment file %s"%file_to_attach
-        print(msg)
-        sys.exit(1)
+encoders.encode_base64(part)
 
-    emails = to_emails + cc_emails
-    server = smtplib.SMTP(host)
-    server.sendmail(from_addr,emails,msg.as_string())
-    server.quit()
 
-if __name__ == "__main__":
-    emails = ["kurwapawa@gmail.com","kurwapawa@gmail.com"]
-    cc_emails = ["kurwapawa@gmail.com"]
-    bbc_emails = ["kurwapawa@gmail.com"]
+part.add_header(
+    "Content-Disposition",
+    f"attachment; filename= {filename}",
+)
 
-    subject = "Test email with attachment from Python"
-    body_text = "this email contains an attachment"
-    path = "fileC.txt"
-send_files(subject, body_text, emails, cc_emails, bbc_emails, path)
+# Добавлнение вложения и конвертация в строку
+message.attach(part)
+text = message.as_string()
+
+# Всё для конекта (указание порта там )
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, text)
